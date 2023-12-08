46f7745b72fb4f97d8744bc4c67c700a02e0fabe
Sharyn Richard <sharyn.richard@gmail.com>
2023-12-07 19:00:51 -0500

    initial commit

---
+++ toggle-sections.js
@@ -0,0 +1,24 @@
+function ToggleSections() {
+    var element = document.getElementById("tutorial_tips");
+    var button_text = document.getElementById("expand-button");
+  
+    console.log("does it contain: ", button_text.innerHTML);
+  
+    var sections = Array.from(document.getElementsByTagName("li")).filter(li => li.id.startsWith("li-step"));
+    console.log("Initial sections: ", sections);
+    sections.push(element);
+    console.log("New sections: ", sections);
+  
+    for (i = 0; i < sections.length; ++i) {
+       each = sections[i];
+       if (each.classList.contains("bx--accordion__item--active")) {
+         each.classList.remove("bx--accordion__item--active");
+         console.log("Initially expanded: ", sections);
+         button_text.innerHTML = "Expand all sections";
+         } else {
+         each.classList.add("bx--accordion__item--active");
+         console.log("Not initially expanded: ", sections);
+         button_text.innerHTML = "Collapse all sections";
+       }
+    }
+}
